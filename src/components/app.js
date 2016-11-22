5db068004cac4e7e5e44dde95276a4208ceb2679
Steve Godin <steve.god@gmail.com>
2016-11-22 15:08:02 -0800

    Solution for blue square exercise

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,12 @@
 import React from 'react';
 // 👇 relative import. After from, make sure to write the path
 // beginning from the current file's path
-import HelloWorld from './hello-world'
+import HelloWorld from './hello-world';
+
+// the name that follows `import` will become a javascript
+// variable that references the imported code from the module
+// following `from`
+import Square from './square';
 
 // export default below is similar to
 // module.exports = function (props) { ...
@@ -11,7 +16,8 @@ export default function (props) {
   // the values of CSS properties must be strings
   var style = {
     display: 'flex',
-    flexFlow: 'column'
+    flexFlow: 'column',
+    alignItems: 'center'
   }
 
   return (
@@ -20,6 +26,7 @@ export default function (props) {
       {/* <HelloWorld name="Babs McButterson" /> */}
       <HelloWorld name="Finkle Butterwoth" />
       <HelloWorld name="Doddy Derington the III" />
+      <Square />
     </div>
   )
 }
